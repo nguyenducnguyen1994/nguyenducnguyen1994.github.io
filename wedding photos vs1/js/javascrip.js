6103e5d56199211eb36599408ba9d8af193dd7f3
nguyenducnguyen1994 <nguyenducnguyen.it.1994@gmail.com>
2020-04-20 23:01:51 +0700

    update Chát

--- wedding photos vs1/js/javascrip.js
+++ wedding photos vs1/js/javascrip.js
@@ -86,6 +86,21 @@ $(document).ready(function() {
   }
 })
 
+var btn = $('#toTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 
 
 
