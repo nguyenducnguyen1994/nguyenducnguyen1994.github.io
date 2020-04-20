de27bd1338d4f25a72c4942b5cc33c9931b44c68
nguyenducnguyen1994 <nguyenducnguyen.it.1994@gmail.com>
2020-04-20 14:46:19 +0700

    delete

--- wedding photos vs1/js/javascrip.js
+++ wedding photos vs1/js/javascrip.js
@@ -87,8 +87,7 @@ $(document).ready(function() {
 })
 
 
-$("#back-to-top").click(function(){return $("body, html").animate({scrollTop:0},400),!1});
-$(function(){$('[data-toggle="tooltip"]').tooltip()});
+
 
 
 
