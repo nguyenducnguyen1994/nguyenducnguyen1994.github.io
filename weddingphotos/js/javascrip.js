031140c37e70d0da122c3bf86be3e9cb2c2a8b05
nguyenducnguyen1994 <nguyenducnguyen.it.1994@gmail.com>
2020-06-10 04:21:37 +0700

    update studio2

--- weddingphotos/js/javascrip.js
+++ weddingphotos/js/javascrip.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+// facebook
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v7.0'
+    });
+};
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
     // ALBUM NỔI BẬT
     $('#carousel-featured-album').owlCarousel({
