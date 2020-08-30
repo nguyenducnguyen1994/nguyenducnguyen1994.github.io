5a490e70d3d6a73fd6331fa0f0ba47e30f8132cb
nguyenducnguyen1994 <nguyenducnguyen.it.1994@gmail.com>
2020-08-30 23:47:51 +0700

    quản lý học viên

---
+++ qlhocvien/javascripts.js
@@ -0,0 +1,15 @@
+
+
+
+function getAdd() {
+    $('#form-add').modal('show')
+}
+
+
+function getEdit() {
+    $('#form-edit').modal('show')
+}
+
+function getDelete() {
+    $('#modal-delete').modal('show')
+}
